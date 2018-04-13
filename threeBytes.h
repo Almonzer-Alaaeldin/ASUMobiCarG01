49776292be925c5b051487f692c388a89ab40a0e
Bassem-Abdelwahab <36202475+Bassem-Abdelwahab@users.noreply.github.com>
2018-04-13 05:17:09 +0200

    Update threeBytes.h

--- threeBytes.h
+++ threeBytes.h
@@ -1,1 +1,25 @@
 
+/*
+  threeBytes.h - Library for efficiently storing instructions mobiCar accurate driving .
+  Created by Bassem Abdelwahab, April 13, 2018.
+  Released on Github.
+*/
+#ifndef threeBytes_h
+#define threeBytes_h
+
+#include "Arduino.h"
+
+class threeBytes
+{
+  public:
+    Morse(float value);
+		Morse();
+		float aquire();
+		void store(float instruction);
+  private:
+    byte Byte1;
+		byte Byte2;
+		byte Byte3;
+};
+
+#endif
