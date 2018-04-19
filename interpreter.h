7670fa2b1103fcf73153b571c3826d076c947b05
Bassem-Abdelwahab <36202475+Bassem-Abdelwahab@users.noreply.github.com>
2018-04-19 04:57:46 +0200

    Update interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -10,19 +10,29 @@
 
 class interpreter
 {
-  public:
-    interpreter(char  inst , int  arg1 , int  arg2);
-		interpreter(long cod);
-		interpreter();
-		long encode(char  inst , int  arg1 , int  arg2);
-		bool decode(long cod , char &inst , int &arg1 , int &arg2);
+  public 
+	interpreter(char  inst , int  arg1 , int  arg2);
+	interpreter(long cod);
+	interpreter();
+	long encode(char  inst , int  arg1 , int  arg2);
+	bool decode(long cod , char &inst , int &arg1 , int &arg2);
+	long encode();
+	bool decode();
+	long getCode();
+	char getInst();
+	int getArg1();
+	int getArg2();
+	bool putCode(long cod);
+	bool putInst(char inst);
+	bool putArg1(int arg1);
+	bool putArg2(int arg2);
   private:
-    long code;
-		char instruction;
-		int argument1;
-		int argument2;
-		int exchange(char inst   , bool  sign1 , bool  sign2);
-		char exchange(int opcode , bool &sign1 , bool &sign2);
+	long code;
+	char instruction;
+	int argument1;
+	int argument2;
+	int exchange(char inst   , bool  sign1 , bool  sign2);
+	char exchange(int opcode , bool &sign1 , bool &sign2);
 };
 
 #endif
